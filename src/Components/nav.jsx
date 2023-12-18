84e45746dd1bf4b99e3b0100f68bbb20cf68700d
aamir <aamirkhan@engineer.com>
2023-12-18 09:33:48 +0500

    created nav mobile friendly

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -2,98 +2,193 @@ import { NavLink } from "react-router-dom";
 import { getTronWeb } from "../utils/contractInit";
 import { store } from "../app/store";
 import { initWeb3, resetPeerDetails } from "../features";
-import {useSelector} from "react-redux";
-import {toast} from "react-toastify";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 import { useState, useEffect } from "react";
 import { autoConnect, getDetails } from "../utils";
 import Cookies from "js-cookie";
-
+import { Cross as Hamburger } from "hamburger-react";
 
 export default function NavBar() {
-	const address = useSelector(state => state.web3Api)?.provider?.defaultAddress.base58;
+	const address = useSelector((state) => state.web3Api)?.provider
+		?.defaultAddress.base58;
 	const [toastMsg, setToastMsg] = useState(null);
+	const [isOpen, setIsOpen] = useState(false);
 
 	const handleLogIn = async () => {
-		if(address){
-			store.dispatch(initWeb3({
-				contract : undefined,
-				token : undefined,
-				provider : undefined
-			}));
+		if (address) {
+			store.dispatch(
+				initWeb3({
+					contract: undefined,
+					token: undefined,
+					provider: undefined,
+				})
+			);
 			setToastMsg("Wallet disconnected");
 			store.dispatch(resetPeerDetails());
 			Cookies.set("connected", "false");
-		}else{
+		} else {
 			const response = await getTronWeb();
-			Cookies.set("connected" , "true", {expires : 1/12});
-			if(typeof response === "boolean"){
+			Cookies.set("connected", "true", { expires: 1 / 12 });
+			if (typeof response === "boolean") {
 				setToastMsg(true);
 				await getDetails();
-			}else{
+			} else {
 				setToastMsg(response);
 			}
 		}
-	}
+	};
 
-	useEffect(()=>{
+	useEffect(() => {
 		// for automatically connecting a wallet if its not locked
 		autoConnect();
-	},[]);
+	}, []);
 
 	return (
 		<>
 			<div className="w-full px-6 h-20 flex justify-between items-center border-black border-2 bg-black/50">
 				<div className="flex justify-start gap-x-3 items-center w-5/12 md:w-3/12">
 					<button
-
-                        onClick={() => {
-                            handleLogIn();
-                        }}
+						onClick={() => {
+							handleLogIn();
+						}}
 						type="button"
-						className={`text-white whitespace-nowrap ${address ? "bg-blue-700 hover:bg-blue-800" : "bg-transparent"}  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700`}
+						className={`text-white whitespace-nowrap ${
+							address
+								? "bg-transparent border-2 border-white hover:bg-blue-800"
+								: "bg-blue-700"
+						}  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:hover:bg-blue-700`}
 					>
-						{address ?   "Log Out" : "connect wallet"}
+						{address ? "Log Out" : "connect wallet"}
 					</button>
-					<p className="text-white text-lg whitespace-nowrap">{address ? [...address].slice(0,5).join("") + "...." + [...address].slice(29,34).join("") : ""}</p>
+					<p className="text-white text-lg whitespace-nowrap hidden lg:block">
+						{address
+							? [...address].slice(0, 5).join("") +"...." +[...address].slice(29, 34).join(""): ""}
+					</p>
 				</div>
 
-				<div className="flex justify-between w-3/12 mr-20 items-center">
-                    <NavLink to={""} className={({isActive})=> `${isActive ? "text-blue-700" : "text-white"} font-bold text-lg`}>Home</NavLink>
-                    <NavLink to={"operations"} className={({isActive})=> `${isActive ? "text-blue-700" : "text-white"} font-bold text-lg`}>Operations</NavLink>
-                    <NavLink to={"dashboard"} className={({isActive})=> `${isActive ? "text-blue-700" : "text-white"} font-bold text-lg`}>Dashboard</NavLink>
+				<div className="lg:flex justify-between w-3/12 gap-10 mr-20 items-center hidden">
+					<NavLink
+						to={""}
+						className={({ isActive }) =>
+							`${
+								isActive ? "text-blue-700" : "text-white"
+							} font-bold text-lg`
+						}
+					>
+						Home
+					</NavLink>
+					<NavLink
+						to={"operations"}
+						className={({ isActive }) =>
+							`${
+								isActive ? "text-blue-700" : "text-white"
+							} font-bold text-lg`
+						}
+					>
+						Operations
+					</NavLink>
+					<NavLink
+						to={"dashboard"}
+						className={({ isActive }) =>
+							`${
+								isActive ? "text-blue-700" : "text-white"
+							} font-bold text-lg`
+						}
+					>
+						Dashboard
+					</NavLink>
+				</div>
+				<div className="lg:hidden z-50">
+					<Hamburger
+						color="#fff"
+						toggle={setIsOpen}
+						toggled={isOpen}
+						rounded
+						label="open menu"
+					/>
 				</div>
 			</div>
 
-			{
-				(()=>{
-					if(typeof toastMsg === "string"){
-						toast.error(`${toastMsg}`, {
-							position: "bottom-left",
-							autoClose: 3000,
-							hideProgressBar: false,
-							closeOnClick: true,
-							pauseOnHover: false,
-							draggable: true,
-							progress: undefined,
-							theme: "dark",
-							})
-							setToastMsg(null);
-					}else if(toastMsg === true){
-						toast.success('wallet connected successfully', {
-							position: "bottom-left",
-							autoClose: 3000,
-							hideProgressBar: false,
-							closeOnClick: true,
-							pauseOnHover: true,
-							draggable: true,
-							progress: undefined,
-							theme: "dark",
-							})
-							setToastMsg(null);
-					}
-				})()
-					
-			}
+			{/* responsive menu. */}
+
+			<div
+				className={`bg-black h-screen lg:hidden w-60 absolute top-0 z-10 translate-x-full right-0 duration-200 ease-in ${
+					isOpen ? "translate-x-0" : "translate-x-full"
+				}`}
+			>
+				<div className="w-full h-60 flex items-center">
+					<div className="flex flex-col items-start justify-around h-40 p-5">
+						<NavLink
+							to={""}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+							className={({ isActive }) =>
+								`${
+									isActive ? "text-blue-700" : "text-white"
+								} font-bold text-lg`
+							}
+						>
+							Home
+						</NavLink>
+						<NavLink
+							to={"operations"}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+							className={({ isActive }) =>
+								`${
+									isActive ? "text-blue-700" : "text-white"
+								} font-bold text-lg`
+							}
+						>
+							Operations
+						</NavLink>
+						<NavLink
+							to={"dashboard"}
+							onClick={() => {
+								setIsOpen(false);
+							}}
+							className={({ isActive }) =>
+								`${
+									isActive ? "text-blue-700" : "text-white"
+								} font-bold text-lg`
+							}
+						>
+							Dashboard
+						</NavLink>
+					</div>
+				</div>
+			</div>
+
+			{(() => {
+				if (typeof toastMsg === "string") {
+					toast.error(`${toastMsg}`, {
+						position: "bottom-left",
+						autoClose: 3000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: false,
+						draggable: true,
+						progress: undefined,
+						theme: "dark",
+					});
+					setToastMsg(null);
+				} else if (toastMsg === true) {
+					toast.success("wallet connected successfully", {
+						position: "bottom-left",
+						autoClose: 3000,
+						hideProgressBar: false,
+						closeOnClick: true,
+						pauseOnHover: true,
+						draggable: true,
+						progress: undefined,
+						theme: "dark",
+					});
+					setToastMsg(null);
+				}
+			})()}
 		</>
 	);
 }
